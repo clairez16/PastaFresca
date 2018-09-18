4faeda575cbb1fb472f59ecbeef3676911d96702
Claire Ziemendorf <claire.ziemendorf@gmail.com>
2018-09-18 15:48:54 +0200

    added the one-to-many relationship between the user table and the subscription table

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,5 @@
 class Subscription < ApplicationRecord
+  has_many :users
+
   validates :name, :price_per_month, :number_of_packs_per_week, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :subscription
+
   validates :phone_number, uniqueness: true, format: { with: /\A(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}\z/}
   validates :city, :name, length: { minimum: 2 }, format: {with: /\A[a-zA-Z]+(?:[\s-][a-zA-Z]+)*\z/}
 end
